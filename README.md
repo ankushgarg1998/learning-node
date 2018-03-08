4ce2d8baceb643e1c92c57d9e2660556d8853880
ankushgarg1998 <1998anky@gmail.com>
2018-03-09 02:38:02 +0530

    9th March

---
+++ README.md
@@ -0,0 +1,2 @@
+#VidJot
+A simple small app that lets you store video ideas created while learning NodeJS, Express and Mongo DB.
---
+++ app.js
@@ -0,0 +1,162 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+const app = express();
+const port = 5000;
+
+// Map global promise = get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+    // useMongoClient: true
+})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+// Load Idea Model
+require('./models/Idea');
+const Idea = mongoose.model('ideas');
+
+
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+
+// Body Parser middleware
+app.use(methodOverride('_method'));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Flash Middleware
+app.use(flash());
+
+//Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+// Index Route
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// Idea Index Page
+app.get('/ideas', (req, res) => {
+    Idea.find({})
+        .sort({date: 'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas
+            });
+            
+        })
+});
+
+// Edit Idea Form
+app.get('/ideas/edit/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        })
+});
+
+//About Idea Form
+app.get('/ideas/add', (req, res) => {
+    res.render('ideas/add');
+});
+
+// Process Form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+
+    if(!req.body.title) {
+        errors.push({text: 'Please add a Title'});
+    }
+    if(!req.body.details) {
+        errors.push({text: 'Please add a Details'});
+    }
+
+    if(errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details,
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                req.flash('success_msg', 'Video Idea Added')
+                res.redirect('/ideas');
+            });
+    }
+});
+
+// Edit Form Process
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            // new values
+            idea.title = req.body.title,
+            idea.details = req.body.details
+
+            idea.save()
+                .then(idea => {
+                    req.flash('success_msg', 'Video Idea Updated')
+                    res.redirect('/ideas');
+                })
+        });
+});
+
+// Delete Idea
+app.delete('/ideas/:id', (req, res) => {
+    Idea.remove({_id: req.params.id})
+        .then(() => {
+            req.flash('success_msg', 'Video Idea Removed')
+            res.redirect('/ideas');
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server Started on port ${port}`);
+});
