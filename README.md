43f41bf789b74321f5d1e5e4a21bebd9b0e5552b
ankushgarg1998 <1998anky@gmail.com>
2018-03-11 15:19:11 +0530

    Login Functionality Added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-#VidJot
+# VidJot
 A simple small app that lets you store video ideas created while learning NodeJS, Express and Mongo DB.
--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 
 const app = express();
 const port = 5000;
+
+// Load Routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
 
 // Map global promise = get rid of warning
 mongoose.Promise = global.Promise;
@@ -18,12 +27,6 @@ mongoose.connect('mongodb://localhost/vidjot-dev', {
 })
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
-
-// Load Idea Model
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
-
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -36,7 +39,10 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
-// Body Parser middleware
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method Override middleware
 app.use(methodOverride('_method'));
 
 // Express Session Middleware
@@ -70,92 +76,10 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-// Idea Index Page
-app.get('/ideas', (req, res) => {
-    Idea.find({})
-        .sort({date: 'desc'})
-        .then(ideas => {
-            res.render('ideas/index', {
-                ideas: ideas
-            });
-            
-        })
-});
 
-// Edit Idea Form
-app.get('/ideas/edit/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            res.render('ideas/edit', {
-                idea: idea
-            });
-        })
-});
-
-//About Idea Form
-app.get('/ideas/add', (req, res) => {
-    res.render('ideas/add');
-});
-
-// Process Form
-app.post('/ideas', (req, res) => {
-    let errors = [];
-
-    if(!req.body.title) {
-        errors.push({text: 'Please add a Title'});
-    }
-    if(!req.body.details) {
-        errors.push({text: 'Please add a Details'});
-    }
-
-    if(errors.length > 0) {
-        res.render('ideas/add', {
-            errors: errors,
-            title: req.body.title,
-            details: req.body.details
-        });
-    } else {
-        const newUser = {
-            title: req.body.title,
-            details: req.body.details,
-        }
-        new Idea(newUser)
-            .save()
-            .then(idea => {
-                req.flash('success_msg', 'Video Idea Added')
-                res.redirect('/ideas');
-            });
-    }
-});
-
-// Edit Form Process
-app.put('/ideas/:id', (req, res) => {
-    Idea.findOne({
-        _id: req.params.id
-    })
-        .then(idea => {
-            // new values
-            idea.title = req.body.title,
-            idea.details = req.body.details
-
-            idea.save()
-                .then(idea => {
-                    req.flash('success_msg', 'Video Idea Updated')
-                    res.redirect('/ideas');
-                })
-        });
-});
-
-// Delete Idea
-app.delete('/ideas/:id', (req, res) => {
-    Idea.remove({_id: req.params.id})
-        .then(() => {
-            req.flash('success_msg', 'Video Idea Removed')
-            res.redirect('/ideas');
-        });
-});
+// Use Routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 app.listen(port, () => {
     console.log(`Server Started on port ${port}`);
